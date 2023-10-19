46f37afb6624fbb3c34148afd31458f1cb7cde83
panicacid <steve@pcquicktips.net>
2023-10-19 11:55:31 +0100

    Update nasharmoniser.sh
    Improved error handling and also fixed a few issues that were seemingly causing certain file paths to stop special characters escaping.

--- nasharmoniser.sh
+++ nasharmoniser.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
-
+ 
 # Get the current date and time for the log file name
 current_datetime=$(date +"%Y%m%d_%H%M%S")
 log_file="rename_operation_${current_datetime}.log"
+ 
+# Capture both stdout and stderr in the log file
+exec &> >(tee -a "$log_file")
+ 
 continue_all=false  # Global variable to keep track of user choice
-
+ 
 # Function to handle errors
 handle_error() {
     local error_message=$1
-    if $continue_all; then
-        echo "$error_message - Continuing as per user choice" | tee -a $log_file
-        return
-    fi
-    
-    while true; do
-        echo "$error_message"
-        echo "Do you want to (Y)es to continue, (N)o to abort or (A)ll to continue for all future errors? Y/N/A: " | tee -a $log_file
-        read -r user_choice
-        case $user_choice in
-            [Yy]* ) echo "Continuing..." | tee -a $log_file; break;;
-            [Nn]* ) echo "Aborting..." | tee -a $log_file; exit 1;;
-            [Aa]* ) echo "Continuing for all future errors..." | tee -a $log_file; continue_all=true; break;;
-            * ) echo "Please answer with Y, N, or A.";;
-        esac
-    done
+    echo "$error_message"
+    echo "Do you want to (Y) continue, (N) abort, or (A) continue ignoring all future errors? Y/N/A: "
+    read -r user_choice
+ 
+    case $user_choice in
+        [Yy]* ) echo "Continuing..." ;;
+        [Nn]* ) echo "Aborting..."; exit 1 ;;
+        [Aa]* ) echo "Continuing and ignoring all future errors..."; continue_all=true ;;
+        * ) echo "Please answer with Y, N, or A." ;;
+    esac
 }
-
+ 
 # Ensure a directory is provided as an argument
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 DIRECTORY" | tee -a $log_file  # Append the error message to the log file
+    handle_error "Usage: $0 DIRECTORY"
     exit 1
 fi
-
+ 
 # Convert the relative path to an absolute path
-cd "$1" || handle_error "Failed to change directory to $1"  # New error handling call
-
+cd "$1" || handle_error "Failed to change directory to $1"
 DIRECTORY=$(pwd)
-cd - || handle_error "Failed to revert to original directory"  # New error handling call
-
+cd - || handle_error "Failed to revert to original directory"
+ 
 # Function to sanitize the names of files
 sanitize_file_names() {
     local dir=$1
-    find "$dir" -type f | while read -r item; do
+    find "$dir" -type f -print0 | while IFS= read -r -d '' item; do
         local dirpath=$(dirname "$item")
         local basename=$(basename "$item")
         local new_basename=$(echo "$basename" | sed -E 's/^[[:space:]]+/_/g; s/[\\/:*?"<>|]/_/g' | sed 's/ \./_./g')
@@ -52,16 +49,16 @@ sanitize_file_names() {
                 final_basename="${new_basename%.*}_$counter.${new_basename##*.}"
                 counter=$((counter + 1))
             done
-            echo "Renaming file: $item to $dirpath/$final_basename" | tee -a $log_file  # Append info to log file
-            mv "$item" "$dirpath/$final_basename"
+            echo "Renaming file: $item to $dirpath/$final_basename"
+            mv "$item" "$dirpath/$final_basename" || handle_error "Failed to rename $item to $dirpath/$final_basename"
         fi
     done
 }
-
+ 
 # Function to sanitize the names of directories
 sanitize_dir_names() {
     local dir=$1
-    find "$dir" -type d | sort -r | while IFS= read -r item; do
+    find "$dir" -type d -print0 | sort -z -r | while IFS= read -r -d '' item; do
         local dirpath=$(dirname "$item")
         local basename=$(basename "$item")
         local new_basename=$(echo "$basename" | sed -E 's/^[[:space:]]+/_/g; s/[\\/:*?"<>|]/_/g' | sed 's/ \+/ /g' | awk '{gsub(/ +$/,"_"); print}')
@@ -72,12 +69,12 @@ sanitize_dir_names() {
                 final_basename="${new_basename}_$counter"
                 counter=$((counter + 1))
             done
-            echo "Renaming directory: $item to $dirpath/$final_basename" | tee -a $log_file  # Append info to log file
-            mv "$item" "$dirpath/$final_basename"
+            echo "Renaming directory: $item to $dirpath/$final_basename"
+            mv "$item" "$dirpath/$final_basename" || handle_error "Failed to rename $item to $dirpath/$final_basename"
         fi
     done
 }
-
+ 
 # Call the functions
 sanitize_file_names "$DIRECTORY"
 sanitize_dir_names "$DIRECTORY"
